1c80f24931c53864532ee8635ec9035b10371350
Anisa Aden <107184280+anisaaden@users.noreply.github.com>
2022-10-18 13:22:13 +0300

    Delete Backend directory

--- Backend/iam_lambda.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "lambda_policy"
-  role   = aws_iam_role.lambda_role.id
-  policy = file("templates/lambda_policy.json")
-}
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_role"
-  assume_role_policy = file("templates/lambda_assume_role_policy.json")
-}
--- Backend/lambdatrigger_v2.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "python_lambda_package" {  
-  type = "zip"  
-  source_file = "python/lambda_function_v2.py" 
-  output_path = "lambdatrigger_v2.zip"
-}
