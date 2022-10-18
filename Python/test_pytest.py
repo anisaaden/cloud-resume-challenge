f3b495a7db083012d11888255c077dc1fc55c9de
Anisa Aden <107184280+anisaaden@users.noreply.github.com>
2022-10-18 13:34:45 +0300

    Delete Python directory

--- Python/test_pytest.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def test_get_visitor_count_check_status_code_equals_200():
-     response = requests.get("https://wkvkeb54y5.execute-api.us-east-1.amazonaws.com/dev/lambda_function_CRC")
-     assert response.status_code == 200
