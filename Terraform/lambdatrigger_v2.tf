af20917ec12f99d9d2840eaedb7c36804dd79343
Anisa Aden <107184280+anisaaden@users.noreply.github.com>
2022-10-19 11:51:57 +0300

    Update lambdatrigger_v2.tf

--- Terraform/lambdatrigger_v2.tf
+++ Terraform/lambdatrigger_v2.tf
@@ -1,5 +1,5 @@
 data "archive_file" "python_lambda_package" {  
   type = "zip"  
-  source_file = "python/lambda_function_v2.py" 
+  source_file = "${path.module}/python/lambda_function_v2.py" 
   output_path = "lambdatrigger_v2.zip"
 }
