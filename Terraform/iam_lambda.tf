5f309afade874f823c6448e6233d8ec4c86e0d6a
Anisa Aden <107184280+anisaaden@users.noreply.github.com>
2022-10-19 11:47:52 +0300

    Update iam_lambda.tf

--- Terraform/iam_lambda.tf
+++ Terraform/iam_lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role_policy" "lambda_policy" {
   name   = "lambda_policy"
   role   = aws_iam_role.lambda_role.id
-  policy = file("templates/lambda_policy.json")
+  policy = file("${path.module}/Templates/lambda_policy.json")
 }
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
