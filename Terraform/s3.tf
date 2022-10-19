273f71e3e69bfa3128027ec374df333426ce11e2
Anisa Aden <107184280+anisaaden@users.noreply.github.com>
2022-10-19 10:46:19 +0300

    Update s3.tf

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
   acl = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  policy = templatefile("./templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "www_bucket" {
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
   acl = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+  policy = templatefile("./templates/s3-policy.json", { bucket = var.bucket_name })
 
   website {
     redirect_all_requests_to = "https://www.${var.domain_name}"
