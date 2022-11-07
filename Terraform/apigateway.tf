74308f742a63b31e40abe4de60815f2e92f50451
anisaaden <107184280+anisaaden@users.noreply.github.com>
2022-11-07 17:34:40 +0000

    api throttling

--- Terraform/apigateway.tf
+++ Terraform/apigateway.tf
@@ -31,6 +31,27 @@ resource "aws_apigatewayv2_stage" "dev" {
    tags = var.common_tags
 }
 
+resource "aws_api_gateway_usage_plan" "api-plan" {
+  name         = "my-usage-plan"
+  description  = "throttling and rate limiting API"
+
+  api_stages {
+    api_id = aws_apigatewayv2_api.record-update.id
+    stage  = aws_apigatewayv2_stage.dev.name
+  }
+
+  quota_settings {
+    limit  = 50
+    offset = 2
+    period = "WEEK"
+  }
+
+  throttle_settings {
+    burst_limit = 10
+    rate_limit  = 10
+  }
+}
+
 resource "aws_cloudwatch_log_group" "record-update_api_gw" {
   name = "/aws/api-gw/${aws_apigatewayv2_api.record-update.name}"
 
