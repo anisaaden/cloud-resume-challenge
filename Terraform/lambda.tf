0651306c8bc2b2c5278a22c8a7bbc1ea5e229709
Anisa Aden <anisamaden@hotmail.co.uk>
2022-11-30 15:38:34 +0000

    Update lambda.tf

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -1,6 +1,4 @@
 resource "aws_lambda_function" "dynamodb_lambda" {
-  # If the file is not in the current working directory you will need to include a 
-  # path.module in the filename.
   filename      = "lambdatrigger.zip"
   function_name = "lambda_function_CRC"
   role          = aws_iam_role.lambda_role.arn
